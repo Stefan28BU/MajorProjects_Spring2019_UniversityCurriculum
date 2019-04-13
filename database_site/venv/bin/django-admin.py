7acf1916fdb68b2f6a64951a952eb0873b6ae6c6
徐宇帆 <Stefan_Xu@xuyufandeMBP.grandenetworks.net>
2019-04-13 16:27:08 -0500

    first commit

---
+++ database_site/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Stefan_Xu/Desktop/CSI3335/databaseGP/database_site/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
