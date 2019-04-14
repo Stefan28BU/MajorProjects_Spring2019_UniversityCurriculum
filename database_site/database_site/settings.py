ab7e110c3f072e2fa26d405206af490329c86eec
徐宇帆 <Stefan_Xu@xuyufandeMacBook-Pro.local>
2019-04-14 14:19:05 -0500

    mySQL connected

--- database_site/database_site/settings.py
+++ database_site/database_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'database_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'UVC',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
