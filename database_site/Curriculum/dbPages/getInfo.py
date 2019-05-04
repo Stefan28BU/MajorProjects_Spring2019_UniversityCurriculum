bdb77342f386ca8d4ccd7821c6ddaa5beb8d2fb1
RBeaver <RylandBeaver@gmail.com>
2019-05-04 14:46:32 -0500

    Finished get_courses_in_curriculum function

--- database_site/Curriculum/dbPages/getInfo.py
+++ database_site/Curriculum/dbPages/getInfo.py
@@ -10,6 +10,6 @@ def get_curricula_heads():
 
 
 def get_courses_in_curricula(cur_name):
-	course_names = CurriculumCourse.objects.filter(Cur_name=cur_name).values('Course_Name')
-	courses = Course.objects.filter(Course_Name__in)
+	course_names = CurriculumCourse.objects.filter(Cur_name=cur_name)
+	return Course.objects.filter(Course_Name__in=course_names)
 
