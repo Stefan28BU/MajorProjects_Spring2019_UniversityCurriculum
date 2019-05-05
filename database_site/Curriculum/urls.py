b4eeaf1b75ad90a966115695d6419be9589db5d2
徐宇帆 <Stefan_Xu@xuyufandeMacBook-Pro.local>
2019-05-04 19:21:50 -0500

    frontend

--- database_site/Curriculum/urls.py
+++ database_site/Curriculum/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('newCurriculum', views.newCurriculum, name='newCurriculum'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- database_site/Curriculum/views.py
+++ database_site/Curriculum/views.py
@@ -9,6 +9,10 @@ def index(request):
     return render(request=request, template_name="index.html")
 
 
+def newCurriculum(request):
+    return render(request=request, template_name="curriculum/newCurriculum.html")
+
+
 def dashboard(request):
     curricula = Curriculum.objects.order_by('-Cur_name')[:5]
     output = ', '.join([c.Cur_name for c in curricula])
